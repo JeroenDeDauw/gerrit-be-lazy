4995d33c1c6f001f71b4951e5677ae861704fc15
jeroendedauw <jeroendedauw@gmail.com>
2013-10-14 14:30:47 +0200

    Be lazy!

---
+++ beLazy.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+USERNAME="jeroendedauw"
+
+declare -f gerrit
+ gerrit ()
+ {
+    if [ -z "$1" ]; then
+        ARGS="--help";
+    else
+        ARGS="$@";
+    fi;
+    set -x;
+    ssh -p 29418 $USERNAME@gerrit.wikimedia.org "gerrit $ARGS";
+    set +x
+ }
+
+commitIds=$(gerrit query "reviewer:'self' is:open" | grep 'change I' | cut -c8-)
+
+for commitId in $commitIds
+do
+	gerrit set-reviewers $commitId --remove $USERNAME
+done
